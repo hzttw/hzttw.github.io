7df901373f3ba9d7019ba1d4bd06c4e5bb5cef7b
hzttw <zh_01040@163.com>
2023-08-07 23:01:25 +0800

    test commit

--- .vuepress/config.ts
+++ .vuepress/config.ts
@@ -1,5 +1,4 @@
 import { defineUserConfig } from 'vuepress';
-import type { DefaultThemeOptions } from 'vuepress';
 import recoTheme from 'vuepress-theme-reco';
 
 export default defineUserConfig({
@@ -11,10 +10,9 @@ export default defineUserConfig({
     author: 'hzttw',
     authorAvatar: '/head.png',
     docsRepo: 'https://github.com/vuepress-reco/vuepress-theme-reco-next',
-    docsBranch: 'main',
+    docsBranch: 'master',
     docsDir: 'example',
     lastUpdatedText: '',
-    // series 为原 sidebar
     series: {
       '/docs/theme-reco/': [
         {
@@ -39,73 +37,6 @@ export default defineUserConfig({
         ],
       },
     ],
-    bulletin: {
-      body: [
-        {
-          type: 'text',
-          content: `🎉🎉🎉 reco 主题 2.x 已经接近 Beta 版本，在发布 Latest 版本之前不会再有大的更新，大家可以尽情尝鲜了，并且希望大家在 QQ 群和 GitHub 踊跃反馈使用体验，我会在第一时间响应。`,
-          style: 'font-size: 12px;',
-        },
-        {
-          type: 'hr',
-        },
-        {
-          type: 'title',
-          content: 'QQ 群',
-        },
-        {
-          type: 'text',
-          content: `
-          <ul>
-            <li>QQ群1：1037296104</li>
-            <li>QQ群2：1061561395</li>
-            <li>QQ群3：962687802</li>
-          </ul>`,
-          style: 'font-size: 12px;',
-        },
-        {
-          type: 'hr',
-        },
-        {
-          type: 'title',
-          content: 'GitHub',
-        },
-        {
-          type: 'text',
-          content: `
-          <ul>
-            <li><a href="https://github.com/vuepress-reco/vuepress-theme-reco-next/issues">Issues<a/></li>
-            <li><a href="https://github.com/vuepress-reco/vuepress-theme-reco-next/discussions/1">Discussions<a/></li>
-          </ul>`,
-          style: 'font-size: 12px;',
-        },
-        {
-          type: 'hr',
-        },
-        {
-          type: 'buttongroup',
-          children: [
-            {
-              text: '打赏',
-              link: '/docs/others/donate.html',
-            },
-          ],
-        },
-      ],
-    },
-    // commentConfig: {
-    //   type: 'valie',
-    //   // options 与 1.x 的 valineConfig 配置一致
-    //   options: {
-    //     // appId: 'xxx',
-    //     // appKey: 'xxx',
-    //     // placeholder: '填写邮箱可以收到回复提醒哦！',
-    //     // verify: true, // 验证码服务
-    //     // notify: true,
-    //     // recordIP: true,
-    //     // hideComments: true // 隐藏评论
-    //   },
-    // },
   }),
   // debug: true,
 });
